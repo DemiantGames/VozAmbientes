9f2540245a32b26c8d2ab785a8070929bb3e4e5f
Demián <demiandal@gmail.com>
2016-04-22 11:30:31 -0500

    VozAmbientes

---
+++ Assets/Voz.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Voz : MonoBehaviour {
+	private FMOD.Studio.EventInstance voz;
+	private FMOD.Studio.ParameterInstance Reverb;
+	private FMOD.Studio.ParameterInstance Calle;
+	private FMOD.Studio.ParameterInstance Library;
+	private FMOD.Studio.ParameterInstance Lluvia;
+	private FMOD.Studio.ParameterInstance Muro;
+	// Use this for initialization
+	private bool oprimido=false;
+
+	private bool oreverb = false, ocalle=false, olibrary=false, olluvia=false, omuro=false;
+	 
+
+
+	void Start () {
+	voz = FMODUnity.RuntimeManager.CreateInstance ("event:/Vozhumana");
+		voz.getParameter ("Reverb", out Reverb);
+		voz.getParameter ("Calle", out Calle);
+		voz.getParameter ("Muro", out Muro);
+		voz.getParameter ("Library", out Library);
+		voz.getParameter ("Lluvia", out Lluvia);
+	}
+	
+	// Update is called once per frame
+	public void voice(){
+		if (!oprimido) {
+			voz.start();
+			oprimido=true;
+
+		} else {
+			
+			voz.stop (FMOD.Studio.STOP_MODE.IMMEDIATE);
+			oprimido=false;
+
+		}
+	}
+
+	public void lluvia(){
+		
+		if (!olluvia) {
+
+			olluvia=true;
+			voz.setParameterValue ("Lluvia", 1);
+
+		} else {
+
+
+			olluvia = false;
+			voz.setParameterValue ("Lluvia", 0);
+		}
+
+	}
+
+	public void biblioteca(){
+		if (!olibrary) {
+
+			olibrary=true;
+			voz.setParameterValue ("Library", 1);
+
+		} else {
+
+
+			olibrary = false;
+			voz.setParameterValue ("Library", 0);
+		}
+		
+	}
+
+
+
+	public void reverb(){
+		if (!oreverb) {
+
+			oreverb=true;
+			voz.setParameterValue ("Reverb", 1);
+
+		} else {
+
+
+			oreverb = false;
+			voz.setParameterValue ("Reverb", 0);
+		}
+
+	}
+	public void calle(){
+
+		if (!ocalle) {
+
+			ocalle=true;
+			voz.setParameterValue ("Calle", 1);
+
+		} else {
+
+
+			ocalle = false;
+			voz.setParameterValue ("Calle", 0);
+		}
+
+	}
+
+
+	public void muro(){
+
+		if (!omuro) {
+
+			omuro=true;
+			voz.setParameterValue ("Muro", 1);
+
+		} else {
+
+
+			omuro = false;
+			voz.setParameterValue ("Muro", 0);
+		}
+
+	}
+
+}
---
+++ Assets/example.cs
@@ -0,0 +1,76 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+
+public class example : MonoBehaviour {
+	private FMOD.Studio.EventInstance velnave;
+	private FMOD.Studio.EventInstance giro;
+	private FMOD.Studio.EventInstance Bass;
+	private FMOD.Studio.EventInstance Laser;
+	private FMOD.Studio.EventInstance MusicLenta;
+	private FMOD.Studio.EventInstance MusicMedia;
+	private FMOD.Studio.EventInstance MusicRapida;
+	private FMOD.Studio.ParameterInstance pitch;
+
+
+
+	private float maxSpeed = 10;
+	public float current_speed=0;
+
+	void Start()
+	{
+
+		velnave = FMODUnity.RuntimeManager.CreateInstance ("event:/Movimiento Spaceship");
+		giro = FMODUnity.RuntimeManager.CreateInstance ("event:/Spaceship_Takeoff-Richard-902554369");
+		Bass = FMODUnity.RuntimeManager.CreateInstance ("event:/Bass Spaceship");
+		Laser = FMODUnity.RuntimeManager.CreateInstance ("event:/Laser");
+		MusicLenta=FMODUnity.RuntimeManager.CreateInstance ("event:/Music");
+		MusicMedia=FMODUnity.RuntimeManager.CreateInstance ("event:/Music3");
+		MusicRapida=FMODUnity.RuntimeManager.CreateInstance ("event:/Music4");
+
+
+		MusicLenta.start ();
+		MusicMedia.start ();
+		MusicRapida.start ();
+		Bass.start ();
+		velnave.start();
+		velnave.getParameter ("Parameter 1", out pitch);
+	}
+	void Update()
+	{
+		if ((Input.GetKeyDown("up") || Input.GetKeyDown("w")) && (current_speed < maxSpeed)) {
+			current_speed += 1; 
+		} else if ((Input.GetKeyDown("down") || Input.GetKeyDown("s"))  && (current_speed > 0)) {
+			current_speed -= 1; 
+		}
+
+		if (current_speed <= 4) {
+			MusicLenta.setPaused (false);
+			MusicRapida.setPaused (true);
+			MusicMedia.setPaused (true);
+
+		} else if (current_speed > 4 && current_speed <= 7) {
+			MusicRapida.setPaused (true);
+			MusicLenta.setPaused (true);
+			MusicMedia.setPaused (false);
+		} else{
+			MusicMedia.setPaused (true);
+			MusicLenta.setPaused (true);
+			MusicRapida.setPaused (false);
+			MusicLenta.start ();
+		}
+		pitch.setValue(current_speed);
+
+
+		if (Input.GetKeyDown("a") || Input.GetKeyDown ("d") || Input.GetKeyDown ("left") || Input.GetKeyDown ("right")) {
+			giro.start ();
+		} 
+
+		if (Input.GetMouseButtonDown (0)) {
+			Laser.start ();
+		}
+
+	}
+}
